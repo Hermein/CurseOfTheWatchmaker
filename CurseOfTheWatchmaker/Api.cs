cba9ca2f230894bcbf6007933eda30d89428b5e5
Артур <Hermein@users.noreply.github.com>
2018-04-23 22:18:26 +0300

    release

---
+++ CurseOfTheWatchmaker/Api.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CurseOfTheWatchmaker
+{
+    public static class Api
+    {
+        public static string Post(string uri, string post)
+        {
+            var html = string.Empty;
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create(uri);
+                request.Method = "POST";
+                request.Headers.Add("X-Requested-With", "ShockwaveFlash/29.0.0.140");
+                request.Headers.Add("Origin", "https://static3.belka-games.com");
+                request.UserAgent =
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36 OPR/52.0.2871.64";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Accept = "*/*";
+                request.Referer = "https://static3.belka-games.com/wcm/vk/mcmwebloader.6a07a5fe.swf";
+                request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate,sdch");
+                request.Headers.Add(HttpRequestHeader.AcceptLanguage, "ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4");
+                request.ServicePoint.Expect100Continue = false;
+                request.AllowAutoRedirect = true;
+                var encodedPostParams = Encoding.UTF8.GetBytes(post);
+                request.ContentLength = encodedPostParams.Length;
+                request.GetRequestStream().Write(encodedPostParams, 0, encodedPostParams.Length);
+                request.GetRequestStream().Close();
+                var response = (HttpWebResponse)request.GetResponse();
+                var responseStream = response.GetResponseStream();
+                if (response.ContentEncoding.ToLower().Contains("gzip"))
+                {
+                    if (responseStream != null)
+                        responseStream = new GZipStream(responseStream, CompressionMode.Decompress);
+                }
+                else if (response.ContentEncoding.ToLower().Contains("deflate"))
+                    if (responseStream != null)
+                        responseStream = new DeflateStream(responseStream, CompressionMode.Decompress);
+                if (responseStream != null)
+                {
+                    var sRead = new StreamReader(responseStream, Encoding.UTF8);
+                    html = sRead.ReadToEnd();
+                    sRead.Close();
+                }
+            }
+            catch
+            {
+
+            }
+            return html;
+        }
+        public static string Md5(string input)
+        {
+            return Md5(Encoding.UTF8.GetBytes(input));
+        }
+        private static string Md5(byte[] input)
+        {
+            return MD5.Create().ComputeHash(input).Aggregate("", (current, a) => current + a.ToString("x2"));
+        }
+        public static string Get(string url)
+        {
+            var html = string.Empty;
+            try
+            {
+                ServicePointManager.ServerCertificateValidationCallback =
+                    ((sender, certificate, chain, sslPolicyErrors) => true);
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                request.Accept = "*/*";
+                request.UserAgent =
+                    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.204 Safari/534.16";
+                request.ServicePoint.Expect100Continue = false;
+                request.Headers.Add(HttpRequestHeader.AcceptCharset, "windows-1251,utf-8;q=0.7,*;q=0.3");
+                request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate,gzip");
+                request.Headers.Add(HttpRequestHeader.AcceptLanguage, "ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4");
+                request.AllowAutoRedirect = true;
+                request.Timeout = 60000;
+                var response = (HttpWebResponse)request.GetResponse();
+                var responseStream = response.GetResponseStream();
+                if (responseStream != null)
+                {
+                    if (response.ContentEncoding.ToLower().Contains("gzip"))
+                        responseStream = new GZipStream(responseStream, CompressionMode.Decompress);
+                    else if (response.ContentEncoding.ToLower().Contains("deflate"))
+                        responseStream = new DeflateStream(responseStream, CompressionMode.Decompress);
+                    var sRead = new StreamReader(responseStream, Encoding.UTF8);
+                    html = sRead.ReadToEnd();
+                    sRead.Close();
+                    sRead.Dispose();
+                    responseStream.Close();
+                    responseStream.Dispose();
+                }
+            }
+            catch
+            {
+
+            }
+            return html;
+        }
+        public static string Parse(string maska, string text)
+        {
+            try
+            {
+                var result = new Regex(maska).Match(text).Groups[1].Value;
+                return result == "" ? "" : result;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+    }
+}
--- CurseOfTheWatchmaker/Program.cs
+++ CurseOfTheWatchmaker/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*Copyright Hermein Developer
+ License CC BY-SA 4.0 
+ 23.04.2018 */
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +12,94 @@ namespace CurseOfTheWatchmaker
 {
     class Program
     {
+        public static Random rand = new Random();
         static void Main(string[] args)
         {
+            int i = 0;
+            string uid, auth;
+
+            Console.WriteLine("Enter your viewer_id");
+            uid = Console.ReadLine();
+            if (!int.TryParse(uid, out i))
+            {
+                Console.WriteLine("Wrong viewer_id");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Enter your auth_key");
+            auth = Console.ReadLine();
+            if (auth.Length != 32)
+            {
+                Console.WriteLine("Wrong auth_key");
+                Console.ReadKey();
+                return;
+            }
+            i = 0;
+            Console.WriteLine("Enter Ruby Count (10-10000)");
+            var rubyCnt = Console.ReadLine();
+            if (!int.TryParse(rubyCnt, out i))
+            {
+                Console.WriteLine("Wrong Ruby Count");
+                Console.ReadKey();
+                return;
+            }
+            if (i < 10 || i > 10000)
+            {
+                Console.WriteLine("Wrong Ruby Count");
+                Console.ReadKey();
+                return;
+            }
+
+
+            //auth
+            var vkApi = Api.Get("https://cm-prod-vk.belkatechnologies.com/bejeweled-balancer/abtest_container_proxy_prod?api_url=https://api.vk.com/api.php&api_id=3088991&api_settings=3&viewer_id=" + uid + "&viewer_type=2&sid=&secret=&access_token=&user_id=" + uid + "&group_id=0&is_app_user=1&auth_key=" + auth + "&language=0&parent_language=0&is_secure=1&stats_hash=&ads_app_id=&referrer=unknown&lc_name=&hash=");
+            vkApi = vkApi.Replace(" ", "");
+            var abTest = Api.Parse("initialflashParams.abTest=\"([0-9]+)\";", vkApi);
+            if (string.IsNullOrWhiteSpace(abTest))
+            {
+                Console.WriteLine("Wrong id/auth_key (not auth)");
+                Console.ReadKey();
+                return;
+            }
+            //init
+            var timeS = Api.Parse("initialflashParams.timeS='([0-9a-zA-Z]+)';", vkApi);
+            var _hash = timeS.Substring(timeS.Length - 5, 4);
+            var _timeStart = long.Parse(timeS.Substring(0, timeS.Length - 5));
+            var r = rand.Next(0, 9).ToString();
+            var state = Api.Post("https://wcm-vk.belka-games.com/BUS/adapters/state", "table=state&id=" + uid + "&method=state&game=wcm&sn=vk&s=" + r + Api.Md5(_hash + r + _timeStart / 61 + "state").Substring(3, 4));
+            try
+            {
+                var jt = JObject.Parse(state);
+                var sv = jt["serverVersion"].Value<string>();
+                var sess = jt["sessionId"].Value<long>();
+                var req = jt["requestID"].Value<long>();
+                var ruby = jt["state"]["consumables"]["ruby"].Value<long>();
+                var dopRuby = i + ruby;
+                //crack
+                var post = "set!consumables.ruby!" + dopRuby + "" + sess;//set!res.17321
+                r = rand.Next(0, 9).ToString();
+                var shash = r + Api.Md5(_hash + r + (_timeStart / 61) + "" + req + "state" + post).Substring(3, 4);
+                var pack = Api.Post("https://wcm-vk.belka-games.com/BUS/adapters/pack", "cmd=set!consumables.ruby!" + dopRuby + "&sessionId=" + sess + "&table=state&id=" + uid + "&serverVersion=" + sv + "&method=state&game=wcm&sn=vk&reqId=" + req + "&s=" + shash);
+                if (pack == req.ToString())
+                {
+                    Console.WriteLine("Succsess!!! Please reboot game!");
+                    Console.ReadKey();
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine(pack);
+                    Console.ReadKey();
+                    return;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error!");
+                Console.ReadKey();
+                return;
+            }
         }
+        
     }
 }
